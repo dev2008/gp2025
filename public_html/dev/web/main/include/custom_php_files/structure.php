49083d9942fe21bb34b798b5ee21b9942bef90a7
Alan Milnes <github@gameplan.org.uk>
2025-02-05 16:13:28 +0000

    Preparing to update uploads table

--- public_html/dev/web/main/include/custom_php_files/structure.php
+++ public_html/dev/web/main/include/custom_php_files/structure.php
@@ -1,5 +1,4 @@
 <?php
-//Version 11.4.2 
 // don't delete this line, this must be the first line of your code
 if(!defined('custom_page_from_inclusion')) { die(); }
 $time_start = microtime(true);
@@ -8,7 +7,7 @@ require_once 'g_functions.php';
 //Create basic layout
 $str="";
 $str.= "<div class='w3-panel w3-theme-d5 w3-text-white w3-round-xxlarge '>";
-$str.="<h1>Page header</h1>";
+$str.="<h1><img src=\"images\NCAA_football.png\" alt=\"AAC logo\" width='160' height='95'> Gameplan College Football XYZ Page</h1>";
 $str.= "<div class='w3-panel w3-theme w3-round-large '>";
 output($str);
 
